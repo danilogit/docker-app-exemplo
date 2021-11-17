ea83c5efb4c678e46f0428fc15fed3d7bf0f829e
Danilo L <hello@danilo.cc>
2021-11-17 00:22:17 -0300

    init

---
+++ src/main/java/br/com/wareline/appexemplo/appexemplo/Controllers/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.wareline.appexemplo.appexemplo.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+@RestController
+class HelloController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/hello")
+    String hello() {
+        return "Olá Wareliner! O nome desse servidor é " + env.getProperty("HOSTNAME");
+    }
+
+}
---
+++ src/test/java/br/com/wareline/appexemplo/appexemplo/AppExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wareline.appexemplo.appexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
